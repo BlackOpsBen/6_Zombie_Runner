e2a478a3de5e480d7d8ba8255106de774ab0bef8
Ben Mora <blackopsben@gmail.com>
2020-02-04 16:55:18 -0600

    Debug gizmo added to enemy for new dynamic chase range.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,16 +6,49 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float pursuitRange = 10f;
+    [SerializeField] bool isPursuing = false;
+    [SerializeField] Color selectionColor = Color.red;
+    float gizmoSize;
+
     NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+
     void Start()
     {
+        gizmoSize = chaseRange;
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget < chaseRange)
+        {
+            isPursuing = true;
+        }
+        if (distanceToTarget > pursuitRange)
+        {
+            isPursuing = false;
+        }
+        if (isPursuing)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (isPursuing)
+        {
+            gizmoSize = pursuitRange;
+        }
+        else
+        {
+            gizmoSize = chaseRange;
+        }
+        Gizmos.color = selectionColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
     }
 }
